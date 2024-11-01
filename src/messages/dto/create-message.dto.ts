fadbf8a21857b984eb77506e14a7f4df11773556
Diego Lima <diegogoncalvesreisdelima21@gmail.com>
2024-11-01 09:36:48 -0300

    Validando dados de entrada com useGlobalPipes, ValidationPipe e class-validator

--- src/messages/dto/create-message.dto.ts
+++ src/messages/dto/create-message.dto.ts
@@ -1,5 +1,21 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateMessageDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(5)
+  @MaxLength(255)
   readonly text: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
   readonly from: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
   readonly to: string;
 }
