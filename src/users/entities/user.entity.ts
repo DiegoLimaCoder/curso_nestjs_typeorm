ced8ac21296ae4d0bc46405071ed93f2d1f86ca7
Diego Lima <diegogoncalvesreisdelima21@gmail.com>
2024-11-01 14:51:14 -0300

    Criando um módulo (Resource) para Entity Pessoa  (usuário da nossa aplicação)

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,30 @@
+import { IsEmail } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column({ length: 100 })
+  name: string;
+
+  @Column()
+  @IsEmail()
+  email: string;
+
+  @Column({ length: 255 })
+  passwordHash: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt?: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt?: Date;
+}
