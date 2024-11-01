fbbb21ab2d90c98258e15d9a46ccf492dd6a8876
Diego Lima <diegogoncalvesreisdelima21@gmail.com>
2024-11-01 12:33:19 -0300

    Primeira Entity do TypeOrm no NestJS (primeira tabela na base de dados PSQL)

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
 import { MessagesService } from './messages.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Messages } from './entities/messages.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Messages])],
   controllers: [MessagesController],
   providers: [MessagesService],
 })
