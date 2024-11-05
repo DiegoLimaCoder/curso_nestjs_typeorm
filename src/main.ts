fef6dd4cb37bc0e275c03cee6ea3101aa52f8624
Diego Lima <diegogoncalvesreisdelima21@gmail.com>
2024-11-05 12:23:00 -0300

    Pipes - como, quando e onde usar Pipes de Validação e Transformação de dados

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ParseIntIdPipe } from './common/pipes/parse-int-id.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,8 +9,9 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true, // remove chaves que não estão no DTO
       forbidNonWhitelisted: true, // levanta um erro quando a chave não existir
-      transform: false, // (true) -> tentar transforma os tipos de dados de param e dtos
+      transform: false, // tentar transforma os tipos de dados de param e dtos
     }),
+    new ParseIntIdPipe(),
   );
   await app.listen(3001);
 }
