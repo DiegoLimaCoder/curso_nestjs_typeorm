08d150d5fd44a95ced0ec31e7585ceb549430110
Diego Lima <diegogoncalvesreisdelima21@gmail.com>
2024-11-12 06:46:06 -0300

    Injeção de dependência em Interceptors, Pipes e outras classes (Injectable)

--- src/common/interceptors/timing-connection.interceptor.ts
+++ src/common/interceptors/timing-connection.interceptor.ts
@@ -1,11 +1,18 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
 import { tap } from 'rxjs';
+
+@Injectable()
 export class TimingConnectionInterceptor implements NestInterceptor {
   async intercept(context: ExecutionContext, next: CallHandler<any>) {
     const startTime = Date.now();
     console.log('TimingConnectionInterceptor executado ANTES');
 
-    await new Promise((resolve) => setTimeout(resolve, 10000));
+    //await new Promise((resolve) => setTimeout(resolve, 10000));
 
     return next.handle().pipe(
       tap(() => {
