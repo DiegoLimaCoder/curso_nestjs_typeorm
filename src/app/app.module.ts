c9a6140989773395629b8c8a436b8593f8da7182
Diego Lima <diegogoncalvesreisdelima21@gmail.com>
2024-11-13 13:52:06 -0300

    Middleware - Tenha acesso direto à Request e Response do Servidor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { MessagesModule } from 'src/messages/messages.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from 'src/users/users.module';
+import { SimpleMiddleware } from 'src/common/middlewares/simple.middleware';
+import { OutroMiddleware } from 'src/common/middlewares/outro.middleware';
 
 @Module({
   imports: [
@@ -13,7 +20,7 @@ import { UsersModule } from 'src/users/users.module';
       port: 5432,
       username: 'docker',
       password: 'docker',
-      database: 'cursonestjs',
+      database: 'cursonestjstypeorm',
       autoLoadEntities: true, // carrega as entidades sem precisa especifica-las
       synchronize: true, // Sincroniza com o Database; Não dever ser usado em produção
     }),
@@ -23,4 +30,15 @@ import { UsersModule } from 'src/users/users.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(SimpleMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+    consumer.apply(OutroMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
